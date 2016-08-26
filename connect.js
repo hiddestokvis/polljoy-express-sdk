ed0b5ddc7557403181fda4dcbe3eb7ab534ddced
Hidde Stokvis <hidde@neverhide.nl>
2016-08-26 14:57:20 +0200

    initial commit

---
+++ connect.js
@@ -0,0 +1,235 @@
+'use strict';
+/**
+* @name connect.js
+* @description Connect SDK for polljoy
+* @author Hidde Stokvis <hidde@neverhide.nl>
+*/
+
+const crypto = require('crypto');
+const request = require('request');
+
+class Connect {
+  /**
+  * constructor()
+  * constructs the Connect class
+  *
+  * @param {String} appId
+  */
+  constructor(appId) {
+    this.appId = appId;
+    this.backend = 'https://api.polljoy.com/3.0/poll/';
+  }
+
+  /**
+  * findIp
+  * finds an ip in a Google AppEngine request object
+  *
+  * @param {Object} req: standard express request object
+  */
+  findIp(req) {
+    if (
+     req.headers &&
+     (
+       req.headers['X-Appengine-User-Ip'] ||
+       req.headers['x-appengine-user-ip']
+     )
+    ) {
+      return req.headers['X-Appengine-User-Ip'] || req.headers['x-appengine-user-ip'];
+    } else if (
+     req.connection &&
+     req.connection.remoteAddress
+    ) {
+      return req.connection.remoteAddress;
+    }
+    return false;
+  }
+
+  /**
+  * sha1()
+  * sha1 hash a string
+  *
+  * @param {String} str
+  */
+  sha1(str) {
+    return crypto.createHash('sha1').update(str).digest('hex');
+  }
+
+  /**
+  * getDeviceId()
+  * Get the sha1 encoded device id
+  *
+  * @param {Object} req: request Object
+  */
+  getDeviceId(req) {
+    return req.headers['user-agent'] + this.sha1(this.findIp(req));
+  }
+
+  /**
+  * getDevice()
+  * Get the device type
+  *
+  * @param {Object} req: request Object
+  */
+  getDevice(req) {
+    const agent = req.headers['user-agent'];
+    if (agent.match(/iPad/) || agent.match(/iPhone/) || agent.match(/Android/)) {
+      return 'mobile';
+    }
+    return 'desktop';
+  }
+
+  /**
+  * getOs()
+  * get the OS by user-agent
+  *
+  * @param {Object} req: request Object
+  */
+  getOs(req) {
+    const agent = req.headers['user-agent'];
+    const os = agent.match(/\(([^)]+)\)/);
+    if (os.length > 1) {
+      return os[1];
+    }
+    return false;
+  }
+
+  /**
+  * createEndpoints()
+  * create the polljoy endpoints
+  *
+  * @param {Object} app
+  * @param {String} url: endpoint
+  */
+  createEndpoints(app, url) {
+    app.post(url, (req, res) => {
+      const deviceId = this.getDeviceId(req);
+      if (req.query.register) { // If register query is found
+        if (
+          req.body &&
+          req.body.deviceId &&
+          req.session &&
+          !(req.body.deviceId === req.session.device_id)
+        ) {
+          Reflect.deleteProperty(req.session, 'current_session');
+        }
+        if (!req.session || !req.session.current_session) {
+          const params = {
+            appId: this.appId,
+            deviceId,
+            deviceModel: 'web',
+            osVersion: this.getOs(req),
+          };
+          if (req.body && req.body.deviceId) {
+            Object.assign(params, {
+              deviceId: req.body.deviceId,
+            });
+          }
+          request.post({
+            url: `${this.backend}registerSession.json`,
+            form: params,
+          }, (err, result) => {
+            if (err) {
+              res.status(500).send(err);
+            } else {
+              const data = JSON.parse(result.body);
+              if (data.session) {
+                delete data.session.appId;
+              }
+              Object.assign(req.session, {
+                current_session: JSON.stringify(data),
+                device_id: params.deviceId,
+              });
+              res.send(data);
+            }
+          });
+        } else {
+          res.send(req.session.current_session);
+        }
+        return true;
+      }
+
+      if (req.query.sg) {
+        const defaultArr = {
+          userType: 'Non-Pay',
+          appVersion: '',
+          deviceId: '',
+          level: '',
+          sessionCount: '',
+          timeSinceInstall: '',
+          tags: '',
+        };
+        const params = req.body;
+        for (let i = 0; i < Object.keys(defaultArr); i++) {
+          const key = Object.keys(defaultArr)[i];
+          if (!params[key]) {
+            Object.assign(params, {
+              [key]: defaultArr[key],
+            });
+          }
+        }
+        Object.assign(params, {
+          deviceModel: this.getDevice(req),
+          platform: 'web',
+          osVersion: this.getOs(req),
+        });
+        [
+          'appVersion',
+          'level',
+          'sessionCount',
+          'timeSinceInstall',
+          'tags',
+        ].forEach((key) => {
+          if (params[key].trim() === '') {
+            delete params[key];
+          }
+        });
+        if (!params.deviceId || params.deviceId.length === 0) {
+          params.deviceId = req.session.device_id || deviceId;
+        }
+        request.post({
+          url: `${this.backend}smartget.json`,
+          form: params,
+        }, (err, result) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            const data = JSON.parse(result.body);
+            if (data.polls && typeof data.polls === 'object') {
+              if (data.session) {
+                delete data.session.appId;
+              }
+              for (let i = 0; i < Object.keys(data.polls); i++) {
+                const key = Object.keys(data.polls)[i];
+                delete data.polls[key].PollRequest.appId;
+              }
+            }
+            res.send(data);
+          }
+        });
+        return true;
+      }
+
+      if (req.query.response) {
+        const params = req.body;
+        if (params.deviceId || params.deviceId.length === 0) {
+          params.deviceId = req.session.device_id || deviceId;
+        }
+        request.post({
+          url: `${this.backend}response/${req.query.token}.json`,
+          form: params,
+        }, (err, result) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            const data = JSON.parse(result.body);
+            res.send(data);
+          }
+        });
+        return true;
+      }
+      return true;
+    });
+  }
+}
+
+module.exports = Connect;
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Polljoy express SDK
+
+Add this to your express.js back-end to register polljoy polls. This replaces the ```connect.php``` file currently provided by Polljoy self. **This package was written for node v6+, it uses ES6 classes.**
+
+## Install
+
+Install with npm:
+
+```npm install polljoy-express-sdk --save```
+
+Make sure to use the express-session middleware:
+
+```npm install express-session --save```
+
+```const session = require('express-session');```
+
+```javascript
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+```
+
+include the connector in your app:
+
+```const Polljoy = require('polljoy-express-sdk');```
+
+```javascript
+Polljoy('*** INSERT APP ID ***').createEndpoints(app, '/polljoy');
+```
+
+This will initialize the /polljoy [POST] endpoint. Follow the regular polljoy instruction and replace the connect.php endpoint with the url of your newly created /polljoy endpoint
+
+## Contribute
+
+This package is more or less an exact translation of the connect.php provided by Polljoy. No unit testing is yet introduced. If you would like to contribute to this package, please fork this repository and submit your changes as a pull request. Stars are awarded to contributers who add (mocha) unit testing :) Please respect the linting rules set.
+
+## License
+
+Copyright (c) 2016 Hidde Stokvis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
